f150b589985bcd6ee0ee64695c97055afbacd8fc
Sahej Jain <89766122+Greninja28@users.noreply.github.com>
2024-03-06 17:00:08 +0530

    created the mobile sidebar

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -1,3 +1,4 @@
+import MobileSidebar from "@/components/sidebar/mobile-sidebar";
 import Sidebar from "@/components/sidebar/sidebar";
 import React from "react";
 
@@ -15,6 +16,9 @@ const Layout = ({ children, params }: LayoutProps) => {
   "
     >
       <Sidebar params={params} />
+      <MobileSidebar>
+        <Sidebar params={params} className="w-screen sm:hidden inline-block" />
+      </MobileSidebar>
       <div
         className="dark:border-neutrals/neutrals-12/70
         border-l-[1px]
